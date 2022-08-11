925a316a601048b4c465f8dc39a40f219d5203a9
leanhthai132002 <vanuong132002@gmail.com>
2022-08-11 22:28:30 +0700

    a

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "hhttps://cellphone0103.herokuapp.com"
+    baseURL: "https://cellphone0103.herokuapp.com"
 })
 
 export default instance;
